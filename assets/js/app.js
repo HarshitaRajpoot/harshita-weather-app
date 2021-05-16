de7ce625202a53bdb15ff4fd219a965204f5bcc0
Archit Madankar <architmadankar21@gmail.com>
2021-05-16 15:51:37 +0530

    Bug fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     }
     else {
         // Default city
-        city = "London";
+        city = "Pune";
     }
     // Get and display current date
     date = moment();
